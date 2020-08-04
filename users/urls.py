26e9ff3aa24941aff76b1d5b9d2e7176645b5c07
Shagun <shagunphw@gmail.com>
2020-08-03 23:13:59 -0700

    own logout template sepreate from admin

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 from users import views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='home'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True),
+         name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -9,8 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f'Account Created for {username}')
-            return redirect('register')
+            messages.success(request, f'Your account has been created. Log In')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'home.html')
