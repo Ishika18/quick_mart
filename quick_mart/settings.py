e9fefc24c80157671d342a8c0133243da644b1db
Shagun <shagunphw@gmail.com>
2020-08-05 22:03:08 -0700

    Home and log out buttons with links

--- quick_mart/settings.py
+++ quick_mart/settings.py
@@ -115,10 +115,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
