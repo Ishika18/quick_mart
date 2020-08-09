bb9669415376d79323701af0b8f6f9e270894bc6
Shagun <shagunphw@gmail.com>
2020-08-09 15:20:31 -0700

    diet restriction model

--- algolia_api/models.py
+++ algolia_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DietRestrictions(models.Model):
+    label = models.CharField(max_length=100)
+    show_name = models.CharField(max_length=100)
+    object_id = models.CharField(max_length=100)
