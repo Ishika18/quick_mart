1f97ebec5271968a6f6fe3e8bf31577533e1e1e3
Shagun <shagunphw@gmail.com>
2020-08-09 19:56:50 -0700

    User profile model added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+import json
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    diet_restrictions = models.CharField(max_length=100000, blank=True)
+
+    def set_profile(self, x):
+        self.diet_restrictions = json.dumps(x)
+
+    def get_profile(self):
+        return json.loads(self.diet_restrictions)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
