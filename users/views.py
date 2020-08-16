ec2b981677858b3fb53757bea48a2bcc42d23f8b
Shagun <shagunphw@gmail.com>
2020-08-16 15:13:41 -0700

    post request on click any checkbox without refreshing page

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from self import self
 
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import Profile
 
 
 def register(request):
@@ -19,15 +21,31 @@ def register(request):
 
 @login_required()
 def profile(request):
-    u_form = UserUpdateForm
-    p_form = ProfileUpdateForm
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
     context = {
-        'u_form': u_form,
-        'p_form': p_form
+        'u_form': u_form
     }
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
 
 
 @login_required()
 def preferences(request):
-    return render(request, 'users/preferences.html')
+    if request.method == 'POST':
+        print(request.POST)
+        Profile.set_profile(self, ['test'])
+        # save values from the form
+        # get the check marked boxes
+        # put all info in a list
+        # Profile.set_profile method
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'p_form': p_form
+    }
+    return render(request, 'users/preferences.html', context)
