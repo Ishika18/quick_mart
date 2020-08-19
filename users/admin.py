d0fb56063e686cf3b07d12de10e1ccf1613d899d
Shagun <shagunphw@gmail.com>
2020-08-18 23:12:36 -0700

    signals to create and save users profile

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, DietRestrictions
 
 admin.site.register(Profile)
-# admin.site.register(DietRestrictions)
+admin.site.register(DietRestrictions)
 
 
--- users/models.py
+++ users/models.py
@@ -4,27 +4,27 @@ from django.db.models import ManyToManyField
 from multiselectfield import MultiSelectField
 
 
-# class DietRestrictions(models.Model):
-#     CHOICES = (
-#         ('vegan', 'vegan'),
-#         ('vegetarian', 'vegetarian'),
-#         ('gluten-free', 'gluten-free'),
-#         ('wheat-free', 'wheat-free'),
-#         ('tree-nut-free', 'tree-nut-free'),
-#         ('soy-free', 'soy-free'),
-#         ('shellfish-free', 'shellfish-free'),
-#         ('sesame-free', 'sesame-free'),
-#         ('low-sugar', 'low-sugar'),
-#         ('no-oil-added', 'no-oil-added'),
-#         ('mustard-free', 'mustard-free')
-#     )
-#     diet_restrictions = MultiSelectField(choices=CHOICES)
+class DietRestrictions(models.Model):
+    CHOICES = (
+        ('vegan', 'vegan'),
+        ('vegetarian', 'vegetarian'),
+        ('gluten-free', 'gluten-free'),
+        ('wheat-free', 'wheat-free'),
+        ('tree-nut-free', 'tree-nut-free'),
+        ('soy-free', 'soy-free'),
+        ('shellfish-free', 'shellfish-free'),
+        ('sesame-free', 'sesame-free'),
+        ('low-sugar', 'low-sugar'),
+        ('no-oil-added', 'no-oil-added'),
+        ('mustard-free', 'mustard-free')
+    )
+    diet_restrictions = MultiSelectField(choices=CHOICES)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.CharField(default='test', max_length=100)
-    # diet_restrictions = ManyToManyField(DietRestrictions)
+    diet_restrictions = ManyToManyField(DietRestrictions)
 
     def __str__(self):
         return f'{self.user.username} Profile'
